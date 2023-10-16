d54be40accb40c7f02ba00cfc072ee67b45eced9
IgorBeHolder <isorochan@gmail.com>
2023-10-16 18:30:30 +0300

    Squashed 'anything-llm/' content from commit ac2a0b0a
    git-subtree-dir: anything-llm
    git-subtree-split: ac2a0b0a06d1a9d7f71163940fd3f9cbe81874a1

---
+++ frontend/src/components/Modals/Password/index.jsx
@@ -0,0 +1,121 @@
+import React, { useState, useEffect } from "react";
+import System from "../../../models/system";
+import SingleUserAuth from "./SingleUserAuth";
+import MultiUserAuth from "./MultiUserAuth";
+import {
+  AUTH_TIMESTAMP,
+  AUTH_TOKEN,
+  AUTH_USER,
+} from "../../../utils/constants";
+
+export default function PasswordModal({ mode = "single" }) {
+  return (
+    <div className="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] h-full bg-gray-600 dark:bg-stone-800 flex items-center justify-center">
+      <div className="flex fixed top-0 left-0 right-0 w-full h-full" />
+      <div className="relative w-full max-w-2xl max-h-full">
+        {mode === "single" ? <SingleUserAuth /> : <MultiUserAuth />}
+      </div>
+    </div>
+  );
+}
+
+export function usePasswordModal() {
+  const [auth, setAuth] = useState({
+    loading: true,
+    required: false,
+    mode: "single",
+  });
+
+  useEffect(() => {
+    async function checkAuthReq() {
+      if (!window) return;
+
+      // If the last validity check is still valid
+      // we can skip the loading.
+      if (!System.needsAuthCheck()) {
+        setAuth({
+          loading: false,
+          requiresAuth: false,
+          mode: "multi",
+        });
+        return;
+      }
+
+      const settings = await System.keys();
+      if (settings?.MultiUserMode) {
+        const currentToken = window.localStorage.getItem(AUTH_TOKEN);
+        if (!!currentToken) {
+          const valid = await System.checkAuth(currentToken);
+          if (!valid) {
+            setAuth({
+              loading: false,
+              requiresAuth: true,
+              mode: "multi",
+            });
+            window.localStorage.removeItem(AUTH_USER);
+            window.localStorage.removeItem(AUTH_TOKEN);
+            window.localStorage.removeItem(AUTH_TIMESTAMP);
+            return;
+          } else {
+            setAuth({
+              loading: false,
+              requiresAuth: false,
+              mode: "multi",
+            });
+            return;
+          }
+        } else {
+          setAuth({
+            loading: false,
+            requiresAuth: true,
+            mode: "multi",
+          });
+          return;
+        }
+      } else {
+        // Running token check in single user Auth mode.
+        // If Single user Auth is disabled - skip check
+        const requiresAuth = settings?.RequiresAuth || false;
+        if (!requiresAuth) {
+          setAuth({
+            loading: false,
+            requiresAuth: false,
+            mode: "single",
+          });
+          return;
+        }
+
+        const currentToken = window.localStorage.getItem(AUTH_TOKEN);
+        if (!!currentToken) {
+          const valid = await System.checkAuth(currentToken);
+          if (!valid) {
+            setAuth({
+              loading: false,
+              requiresAuth: true,
+              mode: "single",
+            });
+            window.localStorage.removeItem(AUTH_TOKEN);
+            return;
+          } else {
+            setAuth({
+              loading: false,
+              requiresAuth: false,
+              mode: "single",
+            });
+            return;
+          }
+        } else {
+          setAuth({
+            loading: false,
+            requiresAuth: true,
+            mode: "single",
+          });
+          return;
+        }
+      }
+    }
+    checkAuthReq();
+  }, []);
+
+  return auth;
+}
