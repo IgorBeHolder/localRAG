ccc255fb0a6c62a56e9fb6cbc8b624fe8c05741b
IgorBeHolder <isorochan@gmail.com>
2023-10-16 18:28:39 +0300

    Remove anything-llm subtree

--- anything-llm/docker/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-node /app/server/index.js &
-{ FLASK_ENV=production FLASK_APP=wsgi.py cd collector && gunicorn --timeout 300 --workers 4 --bind localhost:3005 wsgi:api; } &
-wait -n
-exit $?
--- anything-llm/start_docs_proc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# script to run document processor in development mode
-node ./server/index.js &
-{ FLASK_ENV=production FLASK_APP=wsgi.py cd ./collector && gunicorn --timeout 300 --workers 4 --bind 0.0.0.0:3005 wsgi:api; } &
-# wait -n
-exit $?
