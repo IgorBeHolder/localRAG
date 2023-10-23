8ce08b5e725ebaed98b42e206649b13883fe313e
naverstay <navarstay.me@gmail.com>
2023-10-23 08:51:52 -0400

    gitignore update

--- anything-llm/frontend/src/utils/functions.js
+++ anything-llm/frontend/src/utils/functions.js
@@ -5,3 +5,13 @@ export const stringToUnicode = (input) => {
   }
   return result;
 }
+
+export const fixEncoding = (str) => {
+  const bytes = new Uint8Array(str.length);
+  for (let i = 0; i < str.length; i++) {
+    bytes[i] = str.charCodeAt(i);
+  }
+
+  const decoder = new TextDecoder("utf-8");
+  return decoder.decode(bytes);
+}
