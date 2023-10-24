795589cf83b14ca6da89a18ccc859875d23c4f93
IgorBeHolder <isorochan@gmail.com>
2023-10-24 21:49:36 +0300

    wip vllm

--- copy-hf.sh
+++ copy-hf.sh
@@ -1,15 +1,19 @@
 # Constants
-# as we are at localRAG alredy, we can use relative paths:
+# as we are at localRAG already, we can use relative paths:
 BASE_DIR="./model-store/mistralai/Mistral-7B-Instruct-v0.1-AWQ"
-
-BASE_URL="https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-AWQ/blob/main"
-LFS_URL="https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-AWQ/resolve/main"
+BASE_URL="https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-AWQ/resolve/main"
 
 # Create the directory
-sudo mkdir -p $BASE_DIR
+sudo mkdir -p "$BASE_DIR"
 
-# Download regular files
-declare -a files=(".gitattributes" "README.md" "config.json" "generation_config.json" "quant_config.json" "special_tokens_map.json" "tokenizer.json" "tokenizer_config.json")
+# Download files
+declare -a files=(
+  ".gitattributes" "README.md" "config.json" 
+  "generation_config.json" "quant_config.json" 
+  "special_tokens_map.json" "tokenizer.json" 
+  "tokenizer_config.json" "model.safetensors" 
+  "tokenizer.model"
+)
 
 for file in "${files[@]}"
 do
@@ -17,17 +21,5 @@ do
       sudo curl -o "$BASE_DIR/$file" -L "$BASE_URL/$file"
    else
       echo "File $BASE_DIR/$file already exists. Skipping download."
-   fi
-done
-
-# Download Git LFS files
-declare -a lfs_files=("model.safetensors" "tokenizer.model")
-
-for lfs_file in "${lfs_files[@]}"
-do
-   if [ ! -f "$BASE_DIR/$lfs_file" ]; then
-      sudo curl -o "$BASE_DIR/$lfs_file" -L "$LFS_URL/$lfs_file"
-   else
-      echo "File $BASE_DIR/$lfs_file already exists. Skipping download."
    fi
 done
