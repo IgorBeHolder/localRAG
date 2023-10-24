ebd924e91f008de7b562c0d9aeac54a8d34a1b8c
IgorBeHolder <isorochan@gmail.com>
2023-10-24 19:55:21 +0300

    wip vllm

--- start-vllm.sh
+++ start-vllm.sh
@@ -4,7 +4,7 @@
 echo "Building the vllm container..."
 cd vllm/docker && 
 git checkout main &&
-docker-compose -p localrag -it  up  --build
+docker-compose -p localrag  up  --build
 # docker-compose -p localrag up
 ) 
 
