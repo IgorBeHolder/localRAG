4463b16fa28d03333f8e923eaaec105b705bd287
IgorBeHolder <isorochan@gmail.com>
2023-10-24 21:08:29 +0300

    wip vllm

--- vllm/docker/run.sh
+++ vllm/docker/run.sh
@@ -5,18 +5,18 @@
 # MODEL_DIR="$HOME/localRAG/model-store/$MODEL_NAME"
 # echo "MODEL_DIR=$MODEL_DIR"
 # test -d "$MODEL_DIR"
-python3 -O -u -m vllm.entrypoints.openai.api_server \
+# python3 -O -u -m vllm.entrypoints.openai.api_server \
     # --host=$HOST \
     # --port=$PORT \
     # --model=$MODEL_DIR \
-    --host="0.0.0.0" \
-    --port="3003" \
-    --model="/root/localRAG/model-store/mistralai/Mistral-7B-Instruct-v0.1-AWQ" \
-    --quantization="awq"
+    # --host="0.0.0.0" \
+    # --port="3003" \
+    # --model="/root/localRAG/model-store/mistralai/Mistral-7B-Instruct-v0.1-AWQ" \
+    # --quantization="awq"
     # --tokenizer=hf-internal-testing/llama-tokenizer
     # --download-dir ./model-store &&
 
-# python -m vllm.entrypoints.openai.api_server --model mistralai/Mistral-7B-Instruct-v0.1 --download-dir ./model-store
+python -m vllm.entrypoints.openai.api_server --model mistralai/Mistral-7B-Instruct-v0.1-AWQ --download-dir ./model-store --quantization awq
 
 wait -n
 exit $?
