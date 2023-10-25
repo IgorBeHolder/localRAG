1ee9bd9636b8e2fbdf6748a7c09a1dc26d888818
IgorBeHolder <isorochan@gmail.com>
2023-10-25 22:26:56 +0300

    nginx redirect to vllm

--- vllm/docker/run.sh
+++ vllm/docker/run.sh
@@ -19,7 +19,8 @@
 # python -m vllm.entrypoints.openai.api_server --model ../../model-store/mistralai/Mistral-7B-Instruct-v0.1-AWQ --quantization awq
 python -m vllm.entrypoints.openai.api_server \
     --model ../../model-store/mistralai/Mistral-7B-Instruct-v0.1 \
-    --port 3003 --host 0.0.0.0 --quantization awq
+    --port 3003 --host 0.0.0.0 
+    # --quantization awq
 # wait -n
 # exit $?
 
