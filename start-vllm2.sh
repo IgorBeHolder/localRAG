14d55693ca0b59aeab3dfc689d6759c704486f97
IgorBeHolder <isorochan@gmail.com>
2023-10-25 22:13:39 +0300

    nginx redirect to vllm

--- start-vllm2.sh
+++ start-vllm2.sh
@@ -13,13 +13,13 @@ git checkout main
 
 # Use Docker Compose V2 to build and run the container
 # The `--project-name` flag is equivalent to `-p` in Docker Compose V1
-# docker run --gpus all \
-#     -p 3003:3003 \
-#     -v /model-store/:/model-store/ \
-#      ghcr.io/mistralai/mistral-src/vllm:latest \
-#     --host 0.0.0.0 \
-#     --model="/model-store/mistralai/Mistral-7B-Instruct-v0.1-AWQ/"
+docker run --gpus all \
+    -p 3003:3003 \
+    -v /model-store/:/model-store/ \
+     ghcr.io/mistralai/mistral-src/vllm:latest \
+    --host 0.0.0.0 \
+    --model="/model-store/mistralai/Mistral-7B-Instruct-v0.1-AWQ/"
 
-docker-compose -p localrag -f docker-compose-mistral.yml up  --build
+# docker-compose -p localrag -f docker-compose-mistral.yml up  --build
 
 ) 
