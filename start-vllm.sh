459d3a213dcfde094fd07902a257e37a85a5c401
IgorBeHolder <isorochan@gmail.com>
2023-10-26 09:17:13 +0300

    mistral on dedicated image

--- start-vllm.sh
+++ start-vllm.sh
@@ -7,13 +7,15 @@ cd vllm/docker
 
 git checkout main
 
-# docker run --gpus all \
-#     -p 3003:3003 \
-#     -v /model-store/:/model-store/ \
-#      ghcr.io/mistralai/mistral-src/vllm:latest \
-#     --host 0.0.0.0 \
-#     --model="/model-store/mistralai/Mistral-7B-Instruct-v0.1/"
+docker run --gpus all \
+    -p 3003:3003 \
+    -v /model-store/:/model-store/ \
+    --name vllm-cont \
+    ghcr.io/mistralai/mistral-src/vllm:latest \
+    --host 0.0.0.0 \
+    --model="/model-store/mistralai/Mistral-7B-Instruct-v0.1"
 
-docker-compose -p localrag -f docker-compose.yml up  --build
+
+# docker-compose -p localrag -f docker-compose.yml up  --build
 
 ) 
