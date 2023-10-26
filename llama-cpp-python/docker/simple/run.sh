5a887975c90ebcb3aca039b34927d26507288798
IgorBeHolder <isorochan@gmail.com>
2023-10-26 12:30:14 +0300

    Main app return to host servers

--- llama-cpp-python/docker/simple/run.sh
+++ llama-cpp-python/docker/simple/run.sh
@@ -2,10 +2,9 @@
 
 echo $HOST
 echo $PORT
-python3 -m llama_cpp.server \
     # --model model-store/llama-2-7b-chat.Q4_K_M.gguf \
+python3 -m llama_cpp.server \
     --model model-store/mistral-7b-instruct-v0.1.Q4_K_M.gguf \
     --host $HOST \
     --port $PORT \
-    # --n_ctx 4096
-    --n_ctx 2048    
+    --n_ctx 2048
