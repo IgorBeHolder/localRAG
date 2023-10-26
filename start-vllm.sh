030a0c4a45cb0bcb33634a7f3ad321112755a50d
IgorBeHolder <isorochan@gmail.com>
2023-10-26 07:44:21 +0300

    mistral on nvidia

---
+++ start-vllm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+(
+echo "Building the vllm container..."
+
+cd vllm/docker
+
+git checkout main
+
+# docker run --gpus all \
+#     -p 3003:3003 \
+#     -v /model-store/:/model-store/ \
+#      ghcr.io/mistralai/mistral-src/vllm:latest \
+#     --host 0.0.0.0 \
+#     --model="/model-store/mistralai/Mistral-7B-Instruct-v0.1/"
+
+docker-compose -p localrag -f docker-compose.yml up  --build
+
+) 
