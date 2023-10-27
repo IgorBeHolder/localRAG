7120a6044c757d77891ba5e77380c029ad744a96
IgorBeHolder <isorochan@gmail.com>
2023-10-27 10:14:38 +0300

    Reconfigure nginx router

--- start-vllm.sh
+++ start-vllm.sh
@@ -3,20 +3,19 @@
 (
 echo "Building the vllm container..."
 
-# cd vllm/docker
-
-git checkout main
-
-docker run --gpus all \
-    -p 3003:3003 \
-    -v ./model-store/:/model-store/ \
-    --name vllm-cont \
-    ghcr.io/mistralai/mistral-src/vllm:latest \
-    --host 0.0.0.0 \
-    --port 3003 \
-    --model="/model-store/mistralai/Mistral-7B-Instruct-v0.1"
+cd vllm/docker
+docker-compose -p localrag -f docker-compose.yml up  --build
 
 
-# docker-compose -p localrag -f docker-compose.yml up  --build
+# git checkout main
+
+# docker run --gpus all --restart always \
+#     -p 3003:3003 \
+#     -v ./model-store/:/model-store/ \
+#     --name vllm-cont \
+#     ghcr.io/mistralai/mistral-src/vllm:latest \
+#     --host 0.0.0.0 \
+#     --port 3003 \
+#     --model="/model-store/mistralai/Mistral-7B-Instruct-v0.1"
 
 ) 
