16729b484a89b82992e7fb0527f03f4b711eaf88
IgorBeHolder <isorochan@gmail.com>
2023-10-31 19:20:17 +0300

    Fix doc proc host name

--- anything-llm/docker/docker-entrypoint.sh
+++ anything-llm/docker/docker-entrypoint.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# Load environment variables from .env file
-set -a  # automatically export all variables
-source .env
-set +a
-
 node /app/server/index.js &
 
+# Set the bind address based on MODE
 if [ "$MODE" = "production" ]; then
   BIND_ADDRESS="localhost:3005"
 else
   BIND_ADDRESS="0.0.0.0:3005"
 fi
 
-{ FLASK_ENV=production FLASK_APP=wsgi.py cd collector && gunicorn --timeout 300 --workers 4 --bind $BIND_ADDRESS wsgi:api; } &
+# Start Python Flask server
+export FLASK_ENV=production
+export FLASK_APP=wsgi.py
+cd collector && gunicorn --timeout 300 --workers 4 --bind $BIND_ADDRESS wsgi:api &
 
+# Wait for any of the background jobs to exit
 wait -n
+
+# Exit with the status of the last command that exited
 exit $?
