d5f8393263e77532003af6886455c64566ebfe93
IgorBeHolder <isorochan@gmail.com>
2023-11-03 14:18:42 +0300

    Modify the prompt

--- anything-llm/server/utils/AiProviders/openAi/index.js
+++ anything-llm/server/utils/AiProviders/openAi/index.js
@@ -99,7 +99,7 @@ class OpenAi {
       // { role: "system", content: "<s>[INST] You are a helpful assistant. Your answers should be accurate and concise in russian language only.\n\n [/INST]" }, 
       ...chatHistory,
 
-      { role: "user", content: prompt + " [INST] Отвечай на русском языке. [/INST]"},
+      { role: "user", content: prompt + " [INST] Избегай повторов и отвечай на русском языке. [/INST]"},
     ]; //  chat history with the user's   PROMPT at the END
 
     let textResponse;
