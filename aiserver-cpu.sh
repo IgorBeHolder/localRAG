a7af1107424024ef9f504aed1458b3726700e7fc
IgorBeHolder <isorochan@gmail.com>
2023-12-01 18:01:28 +0300

    New scripts for app deploy

---
+++ aiserver-cpu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+# Set the file name as a variable
+tarball_name="sherpa-aiserver-cpu_v0.1.tar"
+
+# save docker images to a tarball
+echo "Saving docker images to a tarball..."
+docker save -o "$tarball_name" \
+anyth:v1 \
+embed:v1 \
+nginx:latest \
+pgembeding:latest \
+llm-server-cpu:v1 &&
+echo "Docker images saved to a tarball."
+
+# Create a compressed tarball
+echo "Compressing tarball..."
+tar -czvf "$tarball_name.gz" \
+"$tarball_name"  \
+./client-files &&
+echo "Tarball compressed."
+
+rm "$tarball_name"
+
+# Generate MD5 checksum and save to a file
+md5sum "$tarball_name.gz" > "md5sum_$tarball_name.gz.txt"
+
+echo "Script completed successfully."
