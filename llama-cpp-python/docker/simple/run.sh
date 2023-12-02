6f6fab50caeb086c67b53010f4384742fee2435a
IgorBeHolder <isorochan@gmail.com>
2023-12-02 23:24:12 +0300

    increase n_ctx 8192

--- llama-cpp-python/docker/simple/run.sh
+++ llama-cpp-python/docker/simple/run.sh
@@ -3,7 +3,7 @@
 python3 -m llama_cpp.server \
     --model model-store/mistral-7b-instruct-v0.1.Q4_K_M.gguf \
     --host $HOST \
-    --port $PORT
+    --port $PORT \
     --n_ctx 8192
 
 # --model model-store/llama-2-7b-chat.Q4_K_M.gguf \
