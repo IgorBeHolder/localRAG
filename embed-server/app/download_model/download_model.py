d21feb979cc0c06dcdb78882a571c542f3e7a177
IgorBeHolder <isorochan@gmail.com>
2023-12-06 18:29:35 +0300

    Test openchat3.5

---
+++ embed-server/app/download_model/download_model.py
@@ -0,0 +1,20 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import os
+
+def download_model(model_name, save_directory):
+    # Check if the directory exists, if not create it
+    if not os.path.exists(save_directory):
+        os.makedirs(save_directory)
+
+    # Download and cache the model and tokenizer
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    # Save the model and the tokenizer to the specified directory
+    model.save_pretrained(save_directory)
+    tokenizer.save_pretrained(save_directory)
+
+if __name__ == "__main__":
+    model_name = "openchat/openchat_3.5"
+    save_directory = "/model-store/openchat_3.5"
+    download_model(model_name, save_directory)
