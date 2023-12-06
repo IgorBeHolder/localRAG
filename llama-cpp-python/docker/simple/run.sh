ef3a20237f130432c311163f993e9a2cff1c7d4f
IgorBeHolder <isorochan@gmail.com>
2023-12-06 16:17:17 +0300

    Test openchat3.5

--- llama-cpp-python/docker/simple/run.sh
+++ llama-cpp-python/docker/simple/run.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+set -a
+source ./client-files/.env
+set +a
+echo -e "Environment variables loaded.\nBuild for $DEVICE.\n$COMPLETION_MODEL_NAME\n-----------------------------"
+
 
 python3 -m llama_cpp.server \
-    --model model-store/mistral-7b-instruct-v0.1.Q4_K_M.gguf \
+    --model "$COMPLETION_MODEL_NAME" \
     --host $HOST \
     --port $PORT \
     --n_ctx 2048
 
 # --model model-store/llama-2-7b-chat.Q4_K_M.gguf \
 # --model model-store/mistral-7b-instruct-v0.1.Q4_K_M.gguf \
+# --model model-store/openchat/openchat_3.5 \
