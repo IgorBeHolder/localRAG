4e9c94eacaa4741700546f896cf3f6f068073fb3
nv <naverstay.me@gmail.com>
2023-12-07 10:23:33 +0100

    max chat length

--- anything-llm/frontend/src/components/DefaultChat/index.jsx
+++ anything-llm/frontend/src/components/DefaultChat/index.jsx
@@ -6,7 +6,7 @@ import NewWorkspaceModal, {
 import {isMobile} from "react-device-detect";
 import ChatBubble from "../ChatBubble";
 import System from "../../models/system";
-import {CHAT_MAX_LENGTH} from "../../utils/constants.js";
+const CHAT_MAX_LENGTH = 16384;
 
 export default function DefaultChatContainer() {
   const [mockMsgs, setMockMessages] = useState([]);
