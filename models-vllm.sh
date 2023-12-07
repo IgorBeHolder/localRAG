1414283e6f5d327b31fb7ac211fe28221bc85d0f
IgorBeHolder <isorochan@gmail.com>
2023-12-07 12:10:48 +0300

    Deploy with cpu

--- models-vllm.sh
+++ models-vllm.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Set the file name as a variable
-tarball_name="models_vllm_v0.1.tar.gz"
+tarball_name="models_vllm_v0.2.tar.gz"
 
 # Create a compressed tarball
 echo "Compressing tarball..."
