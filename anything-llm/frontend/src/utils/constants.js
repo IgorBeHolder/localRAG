6e37041fe97b0992aeb1977f6ee12ce8afc57105
BeHolder <102916769+IgorBeHolder@users.noreply.github.com>
2023-12-07 11:10:46 +0300

    Revert "Chat max length"

--- anything-llm/frontend/src/utils/constants.js
+++ anything-llm/frontend/src/utils/constants.js
@@ -1,5 +1,4 @@
 export const API_BASE = import.meta.env.VITE_API_BASE || "/api";
-export const CHAT_MAX_LENGTH = parseInt(import.meta.env.VITE_CHAT_MAX_LENGTH || '2048');
 
 export const AUTH_USER = "anythingllm_user";
 export const AUTH_TOKEN = "anythingllm_authToken";
