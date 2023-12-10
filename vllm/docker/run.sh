6a7cd18909d3e0942fd5e9bb14f6cdcf626666b3
IgorBeHolder <isorochan@gmail.com>
2023-12-10 21:45:04 +0300

    openchat.gguf config

--- vllm/docker/run.sh
+++ vllm/docker/run.sh
@@ -17,7 +17,7 @@
     # --download-dir ./model-store &&
 
 python -m vllm.entrypoints.openai.api_server \
-    --model "../../$COMPLETION_MODEL_NAME" \
+    --model "../../model-store/$COMPLETION_MODEL_NAME" \
     --port $MM_PORT \
     --host $HOST
 
