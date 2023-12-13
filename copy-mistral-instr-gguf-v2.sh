57359c2dd1c90c3586c8ff2c8508a08276b599ff
IgorBeHolder <isorochan@gmail.com>
2023-12-13 11:57:12 +0300

    add mistral v2 gguf

--- copy-mistral-instr-gguf-v2.sh
+++ copy-mistral-instr-gguf-v2.sh
@@ -1,7 +1,7 @@
 # Constants
 # as we are at localRAG already, we can use relative paths:
-BASE_DIR="./model-store/mistralai/Mistral-7B-Instruct-v0.1-AWQ"
-BASE_URL="https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.1-AWQ/resolve/main"
+BASE_DIR="./model-store/TheBloke/Mistral-7B-Instruct-v0.2-GGUF"
+BASE_URL="https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main"
 
 # Create the directory
 sudo mkdir -p "$BASE_DIR"
@@ -9,11 +9,8 @@ sudo chmod 755 "$BASE_DIR"
 
 # Download files and set permissions
 declare -a files=(
-  ".gitattributes" "README.md" "config.json" 
-  "generation_config.json" "quant_config.json" 
-  "special_tokens_map.json" "tokenizer.json" 
-  "tokenizer_config.json" "model.safetensors" 
-  "tokenizer.model"
+  ".gitattributes" "README.md" "config.json"
+  "mistral-7b-instruct-v0.2.Q4_K_M.gguf"
 )
 
 for file in "${files[@]}"
