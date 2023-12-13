c67484829e3ba16e8f584e03a796de6e396bdbfc
naverstay <navarstay.me@gmail.com>
2023-12-13 22:22:13 +0100

    NodeSSH

--- anything-llm/frontend/src/components/WorkspaceChat/ChatContainer/PromptInput/index.jsx
+++ anything-llm/frontend/src/components/WorkspaceChat/ChatContainer/PromptInput/index.jsx
@@ -150,7 +150,7 @@ const Tracking = memo(({workspaceSlug}) => {
   return (
     <div className="flex flex-col md:flex-row w-full justify-center items-center gap-2 mb-2 px-4 mx:px-0">
       <p
-        className="bg-gray-200 dark:bg-stone-600 text-gray-800 dark:text-slate-400 text-xs px-2 rounded-lg font-mono text-center">
+        className="whitespace-nowrap bg-gray-200 dark:bg-stone-600 text-gray-800 dark:text-slate-400 text-xs px-2 rounded-lg font-mono text-center">
         Режим чата: {chatMode}
       </p>
       <p className="text-slate-400 text-xs text-center">
