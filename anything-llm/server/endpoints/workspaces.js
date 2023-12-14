cf2a9f7bb11f3ad8e62205bbbcbe6e6816e04ada
naverstay <navarstay.me@gmail.com>
2023-12-14 13:08:14 +0100

    analyst server

--- anything-llm/server/endpoints/workspaces.js
+++ anything-llm/server/endpoints/workspaces.js
@@ -1,19 +1,19 @@
-const { reqBody, multiUserMode, userFromSession } = require("../utils/http");
-const { Workspace } = require("../models/workspace");
-const { Document } = require("../models/documents");
-const { DocumentVectors } = require("../models/vectors");
-const { WorkspaceChats } = require("../models/workspaceChats");
-const { convertToChatHistory } = require("../utils/chats");
-const { getVectorDbClass } = require("../utils/helpers");
-const { setupMulter } = require("../utils/files/multer");
+const {reqBody, multiUserMode, userFromSession} = require("../utils/http");
+const {Workspace} = require("../models/workspace");
+const {Document} = require("../models/documents");
+const {DocumentVectors} = require("../models/vectors");
+const {WorkspaceChats} = require("../models/workspaceChats");
+const {convertToChatHistory} = require("../utils/chats");
+const {getVectorDbClass} = require("../utils/helpers");
+const {setupMulter} = require("../utils/files/multer");
 const {
   checkPythonAppAlive,
-  processDocument,
+  processDocument
 } = require("../utils/files/documentProcessor");
-const { validatedRequest } = require("../utils/middleware/validatedRequest");
-const { SystemSettings } = require("../models/systemSettings");
-const { Telemetry } = require("../models/telemetry");
-const { handleUploads } = setupMulter();
+const {validatedRequest} = require("../utils/middleware/validatedRequest");
+const {SystemSettings} = require("../models/systemSettings");
+const {Telemetry} = require("../models/telemetry");
+const {handleUploads} = setupMulter();
 
 function workspaceEndpoints(app) {
   if (!app) return;
@@ -21,14 +21,14 @@ function workspaceEndpoints(app) {
   app.post("/workspace/new", [validatedRequest], async (request, response) => {
     try {
       const user = await userFromSession(request, response);
-      const { name = null } = reqBody(request);
-      const { workspace, message } = await Workspace.new(name, user?.id);
+      const {name = null} = reqBody(request);
+      const {workspace, message} = await Workspace.new(name, user?.id);
       await Telemetry.sendTelemetry("workspace_created", {
         multiUserMode: multiUserMode(response),
         LLMSelection: process.env.LLM_PROVIDER || "openai",
-        VectorDbSelection: process.env.VECTOR_DB || "pinecone",
+        VectorDbSelection: process.env.VECTOR_DB || "pinecone"
       });
-      response.status(200).json({ workspace, message });
+      response.status(200).json({workspace, message});
     } catch (e) {
       console.log(e.message, e);
       response.sendStatus(500).end();
@@ -41,22 +41,22 @@ function workspaceEndpoints(app) {
     async (request, response) => {
       try {
         const user = await userFromSession(request, response);
-        const { slug = null } = request.params;
+        const {slug = null} = request.params;
         const data = reqBody(request);
         const currWorkspace = multiUserMode(response)
-          ? await Workspace.getWithUser(user, { slug })
-          : await Workspace.get({ slug });
+          ? await Workspace.getWithUser(user, {slug})
+          : await Workspace.get({slug});
 
         if (!currWorkspace) {
           response.sendStatus(400).end();
           return;
         }
 
-        const { workspace, message } = await Workspace.update(
+        const {workspace, message} = await Workspace.update(
           currWorkspace.id,
           data
         );
-        response.status(200).json({ workspace, message });
+        response.status(200).json({workspace, message});
       } catch (e) {
         console.log(e.message, e);
         response.sendStatus(500).end();
@@ -68,7 +68,7 @@ function workspaceEndpoints(app) {
     "/workspace/:slug/upload",
     handleUploads.single("file"),
     async function (request, response) {
-      const { originalname } = request.file;
+      const {originalname} = request.file;
       const processingOnline = await checkPythonAppAlive();
 
       if (!processingOnline) {
@@ -76,15 +76,15 @@ function workspaceEndpoints(app) {
           .status(500)
           .json({
             success: false,
-            error: `Python processing API is not online. Document ${originalname} will not be processed automatically.`,
+            error: `Python processing API is not online. Document ${originalname} will not be processed automatically.`
           })
           .end();
         return;
       }
 
-      const { success, reason } = await processDocument(originalname);
+      const {success, reason} = await processDocument(originalname);
       if (!success) {
-        response.status(500).json({ success: false, error: reason }).end();
+        response.status(500).json({success: false, error: reason}).end();
         return;
       }
 
@@ -92,7 +92,7 @@ function workspaceEndpoints(app) {
         `Document ${originalname} uploaded processed and successfully. It is now available in documents.`
       );
       await Telemetry.sendTelemetry("document_uploaded");
-      response.status(200).json({ success: true, error: null });
+      response.status(200).json({success: true, error: null});
     }
   );
 
@@ -102,11 +102,11 @@ function workspaceEndpoints(app) {
     async (request, response) => {
       try {
         const user = await userFromSession(request, response);
-        const { slug = null } = request.params;
-        const { adds = [], deletes = [] } = reqBody(request);
+        const {slug = null} = request.params;
+        const {adds = [], deletes = []} = reqBody(request);
         const currWorkspace = multiUserMode(response)
-          ? await Workspace.getWithUser(user, { slug })
-          : await Workspace.get({ slug });
+          ? await Workspace.getWithUser(user, {slug})
+          : await Workspace.get({slug});
 
         if (!currWorkspace) {
           response.sendStatus(400).end();
@@ -115,8 +115,8 @@ function workspaceEndpoints(app) {
 
         await Document.removeDocuments(currWorkspace, deletes);
         await Document.addDocuments(currWorkspace, adds);
-        const updatedWorkspace = await Workspace.get({ id: currWorkspace.id });
-        response.status(200).json({ workspace: updatedWorkspace });
+        const updatedWorkspace = await Workspace.get({id: currWorkspace.id});
+        response.status(200).json({workspace: updatedWorkspace});
       } catch (e) {
         console.log(e.message, e);
         response.sendStatus(500).end();
@@ -129,12 +129,12 @@ function workspaceEndpoints(app) {
     [validatedRequest],
     async (request, response) => {
       try {
-        const { slug = "" } = request.params;
+        const {slug = ""} = request.params;
         const user = await userFromSession(request, response);
         const VectorDb = getVectorDbClass();
         const workspace = multiUserMode(response)
-          ? await Workspace.getWithUser(user, { slug })
-          : await Workspace.get({ slug });
+          ? await Workspace.getWithUser(user, {slug})
+          : await Workspace.get({slug});
 
         if (!workspace) {
           response.sendStatus(400).end();
@@ -143,7 +143,7 @@ function workspaceEndpoints(app) {
 
         if (multiUserMode(response) && user.role !== "admin") {
           const canDelete =
-            (await SystemSettings.get({ label: "users_can_delete_workspaces" }))
+            (await SystemSettings.get({label: "users_can_delete_workspaces"}))
               ?.value === "true";
           if (!canDelete) {
             response.sendStatus(500).end();
@@ -151,13 +151,13 @@ function workspaceEndpoints(app) {
           }
         }
 
-        await WorkspaceChats.delete({ workspaceId: Number(workspace.id) });
+        await WorkspaceChats.delete({workspaceId: Number(workspace.id)});
         await DocumentVectors.deleteForWorkspace(workspace.id);
-        await Document.delete({ workspaceId: Number(workspace.id) });
-        await Workspace.delete({ id: Number(workspace.id) });
+        await Document.delete({workspaceId: Number(workspace.id)});
+        await Workspace.delete({id: Number(workspace.id)});
 
         try {
-          await VectorDb["delete-namespace"]({ namespace: slug });
+          await VectorDb["delete-namespace"]({namespace: slug});
         } catch (e) {
           console.error(e.message);
         }
@@ -176,7 +176,7 @@ function workspaceEndpoints(app) {
         ? await Workspace.whereWithUser(user)
         : await Workspace.where();
 
-      response.status(200).json({ workspaces });
+      response.status(200).json({workspaces});
     } catch (e) {
       console.log(e.message, e);
       response.sendStatus(500).end();
@@ -184,14 +184,15 @@ function workspaceEndpoints(app) {
   });
 
   app.get("/workspace/:slug", [validatedRequest], async (request, response) => {
+    console.log("workspace", request);
     try {
-      const { slug } = request.params;
+      const {slug} = request.params;
       const user = await userFromSession(request, response);
       const workspace = multiUserMode(response)
-        ? await Workspace.getWithUser(user, { slug })
-        : await Workspace.get({ slug });
+        ? await Workspace.getWithUser(user, {slug})
+        : await Workspace.get({slug});
 
-      response.status(200).json({ workspace });
+      response.status(200).json({workspace});
     } catch (e) {
       console.log(e.message, e);
       response.sendStatus(500).end();
@@ -203,11 +204,11 @@ function workspaceEndpoints(app) {
     [validatedRequest],
     async (request, response) => {
       try {
-        const { slug } = request.params;
+        const {slug} = request.params;
         const user = await userFromSession(request, response);
         const workspace = multiUserMode(response)
-          ? await Workspace.getWithUser(user, { slug })
-          : await Workspace.get({ slug });
+          ? await Workspace.getWithUser(user, {slug})
+          : await Workspace.get({slug});
 
         if (!workspace) {
           response.sendStatus(400).end();
@@ -218,7 +219,7 @@ function workspaceEndpoints(app) {
           ? await WorkspaceChats.forWorkspaceByUser(workspace.id, user.id)
           : await WorkspaceChats.forWorkspace(workspace.id);
 
-        response.status(200).json({ history: convertToChatHistory(history) });
+        response.status(200).json({history: convertToChatHistory(history)});
       } catch (e) {
         console.log(e.message, e);
         response.sendStatus(500).end();
@@ -227,4 +228,4 @@ function workspaceEndpoints(app) {
   );
 }
 
-module.exports = { workspaceEndpoints };
+module.exports = {workspaceEndpoints};
