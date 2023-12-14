db4f5a491b0b1ea842a132180d2143dbfbbf8a09
IgorBeHolder <isorochan@gmail.com>
2023-12-14 19:28:00 +0300

    openchat_3.5-16k.Q4_K_M.gguf

---
+++ aiserver-cpu-RTC.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+# Set the file name as a variable
+tarball_name="sherpa-aiserver-RTC.tar"
+
+# save docker images to a tarball
+echo "Saving docker images to a tarball..."
+docker save -o "$tarball_name" \
+anyth:v1 \
+embed:v1 \
+nginx:latest \
+pgembeding:latest \
+llm-server-cpu:v2 &&
+echo "Docker images saved to a tarball."
+
+# Create a compressed tarball
+echo "Compressing tarball..."
+tar -czvf "$tarball_name.gz" \
+"$tarball_name"  \
+./start.sh \
+./start-em.sh \
+./start-mm.sh \
+./start-vllm.sh \
+./db-reset.sh \
+./client-files &&
+echo "Tarball compressed."
+
+rm "$tarball_name"
+
+# Generate MD5 checksum and save to a file
+md5sum "$tarball_name.gz" > "md5sum_$tarball_name.gz.txt"
+
+echo "Script completed successfully."
+
+echo "Script completed successfully."
