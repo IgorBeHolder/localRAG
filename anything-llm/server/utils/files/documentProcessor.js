e8a18c0f494e7c2a0730469c7c9e2cf50f6aa9e7
naverstay <navarstay.me@gmail.com>
2023-12-17 14:19:26 +0100

    MangeCoder

--- anything-llm/server/utils/files/documentProcessor.js
+++ anything-llm/server/utils/files/documentProcessor.js
@@ -32,7 +32,28 @@ async function acceptedFileTypes() {
 
 async function processDocument(filename = "") {
   if (!filename) return false;
-  // send filename to python app: 
+  // send filename to python app:
+  return await fetch(`${PYTHON_API}/process`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ filename }),
+  })
+    .then((res) => {
+      if (!res.ok) throw new Error("Запрос не удался");
+      return res.json();
+    })
+    .then((res) => res)
+    .catch((e) => {
+      console.log(e.message);
+      return { success: false, reason: e.message };
+    });
+}
+
+async function processCsvDocument(filename = "") {
+  if (!filename) return false;
+  // send filename to python app:
   return await fetch(`${PYTHON_API}/process`, {
     method: "POST",
     headers: {
