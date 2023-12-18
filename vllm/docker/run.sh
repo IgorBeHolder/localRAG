1cae8dee9e1070076f663862138bcc869ef1871d
IgorBeHolder <isorochan@gmail.com>
2023-12-18 20:40:56 +0300

    tune vllm setup

--- vllm/docker/run.sh
+++ vllm/docker/run.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
-# /root/localRAG/model-store/mistralai/Mistral-7B-Instruct-v0.1-AWQ
-# MODEL_NAME="$1"
-# test -n "$MODEL_NAME"
-# MODEL_DIR="$HOME/localRAG/model-store/$MODEL_NAME"
-# echo "MODEL_DIR=$MODEL_DIR"
-# test -d "$MODEL_DIR"
-# python3 -O -u -m vllm.entrypoints.openai.api_server \
-    # --host=$HOST \
-    # --port=$PORT \
-    # --model=$MODEL_DIR \
-    # --host="0.0.0.0" \
-    # --port="3003" \
-    # --model="../../model-store/mistralai/Mistral-7B-Instruct-v0.1-AWQ" \
-    # --quantization="awq"
-    # --tokenizer=hf-internal-testing/llama-tokenizer
-    # --download-dir ./model-store &&
+
 
 python -m vllm.entrypoints.openai.api_server \
     --model "../../model-store/$COMPLETION_MODEL_NAME" \
