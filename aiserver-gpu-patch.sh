ac186e88e334983fb01c767a9657ce2a01c22233
IgorBeHolder <isorochan@gmail.com>
2023-12-18 22:10:28 +0300

    tune vllm setup

---
+++ aiserver-gpu-patch.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+# Set the file name as a variable
+tarball_name="sherpa-aiserver-openchat_v0.1.tar"
+
+# save docker images to a tarball
+echo "Saving docker images to a tarball..."
+docker save -o "$tarball_name" \
+nginx:latest \
+anyth:v1 
+# embed:v1 \
+# pgembeding:latest \
+# vllm/vllm-openai
+# ghcr.io/mistralai/mistral-src/vllm:latest
+echo "Docker images saved to a tarball."
+
+# Create a compressed tarball
+echo "Compressing tarball..."
+tar -czvf "$tarball_name.gz" \
+"$tarball_name"  \
+# ./vllm/docker/docker-compose.yml \
+# ./embed-server/config/ \
+# ./db-reset.sh \
+# ./client-files/ 
+
+echo "Tarball compressed."
+
+rm "$tarball_name"
+
+# Generate MD5 checksum and save to a file
+md5sum "$tarball_name.gz" > "md5sum_$tarball_name.gz.txt"
+
+echo "md5sum saved to md5sum_$tarball_name.gz.txt"
+
+echo "Script completed successfully."
