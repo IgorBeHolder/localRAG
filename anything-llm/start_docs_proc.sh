99dcaf42a8dfd417068bad731533c90f5ecd4f37
IgorBeHolder <isorochan@gmail.com>
2023-12-21 21:16:01 +0300

    mixtral

--- anything-llm/start_docs_proc.sh
+++ anything-llm/start_docs_proc.sh
@@ -2,5 +2,8 @@
 
 # script to run document processor in development mode
 source ./collector/venv/bin/activate
-{ FLASK_ENV=production FLASK_APP=wsgi.py cd ./collector && gunicorn --timeout 300 --workers 4 --bind 0.0.0.0:3005 wsgi:api; } 
 
+# { FLASK_ENV=production FLASK_APP=wsgi.py cd ./collector && gunicorn --timeout 300 --workers 4 --bind 0.0.0.0:3005 wsgi:api; }
+
+
+{ FLASK_ENV=development FLASK_APP=wsgi.py cd ./collector && gunicorn --timeout 300 --workers 4 --bind 0.0.0.0:3005 wsgi:api; } 
---
+++ copy-mixtral.sh
@@ -0,0 +1,21 @@
+# Constants
+# as we are at localRAG already, we can use relative paths:
+BASE_DIR="./model-store/TheBloke/Mixtral-8x7B-Instruct-v0.1-GGUF"
+BASE_URL="https://huggingface.co/TheBloke/Mixtral-8x7B-Instruct-v0.1-GGUF/resolve/main"
+
+# Create the directory
+sudo mkdir -p "$BASE_DIR"
+sudo chmod 755 "$BASE_DIR"
+
+# Download files and set permissions
+declare -a files=(
+  ".gitattributes" "README.md" "config.json" 
+  "mixtral-8x7b-instruct-v0.1.Q4_K_M.gguf"
+)
+
+for file in "${files[@]}"
+do
+   sudo curl -o "$BASE_DIR/$file" -L "$BASE_URL/$file"
+   sudo chmod 644 "$BASE_DIR/$file"
+   echo "Downloaded and set permissions for $BASE_DIR/$file"
+done
