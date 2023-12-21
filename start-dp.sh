436cbc50689da6f46803408f5a5f860e33751cc3
IgorBeHolder <isorochan@gmail.com>
2023-12-21 12:54:24 +0300

    fix errors after merge

--- start-dp.sh
+++ start-dp.sh
@@ -2,6 +2,6 @@
 
 (
 echo "Starting the docs processor...(hotdir watch)"
-#lsof -i :3005 | awk 'NR>1 {print $2}' | xargs kill -9
+lsof -i :3005 | awk 'NR>1 {print $2}' | xargs kill -9
 cd anything-llm/ && ./start_docs_proc.sh
 ) &
