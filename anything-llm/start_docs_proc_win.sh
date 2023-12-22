69413118f7dd70736541859231104f820427d4a8
nv <naverstay.me@gmail.com>
2023-12-22 22:50:11 +0100

    WS_PORT

--- anything-llm/start_docs_proc_win.sh
+++ anything-llm/start_docs_proc_win.sh
@@ -4,7 +4,7 @@
 collector/venv/scripts/activate
 # cd ..
 # { set FLASK_ENV = "production" && set FLASK_APP="wsgi.py" && cd ./collector && flask run --host=0.0.0.0 --port=3005; }
-{ set FLASK_ENV = "development" && set FLASK_APP="wsgi.py" && cd ./collector && flask run --host=0.0.0.0 --port=3005; }
+#{ $env:FLASK_ENV = "production" && $env:FLASK_APP="wsgi.py" && cd ./collector && flask run --host=0.0.0.0 --port=3005; }
 # wait -n
 # exit $?
 
