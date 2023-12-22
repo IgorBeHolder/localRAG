8d70fa6e226ec4b9d5f37d66953bb89797a894df
IgorBeHolder <isorochan@gmail.com>
2023-12-22 14:58:55 +0300

    tensor_parallel

--- start-llama.sh
+++ start-llama.sh
@@ -6,17 +6,20 @@ set -a
 echo $PWD
 source ./client-files/.env
 set +a
-echo -e "Environment variables loaded.\nBuild for $DEVICE.\n$COMPLETION_MODEL_NAME\n-----------------------------"
+echo -e "Environment variables loaded."
+echo -e "Build for $DEVICE."
+echo -e "$COMPLETION_MODEL_NAME\n-----------------------------"
 
 # Check if the "llm-server" container is already running and remove it if it is
 container_id=$(docker ps -a -q -f name=^/llm-server$)
 if [ ! -z "$container_id" ]; then
     echo "Stopping existing container with name 'llm-server'..."
     docker stop $container_id
+    echo "Removing existing container with name 'llm-server'..."
     docker rm -f $container_id
 fi
 
-if [ $DEVICE="cpu" ]; then
+if [ "$DEVICE" = "cpu" ]; then
     cd llama-cpp-python/docker/simple
     echo "Building the LLAMA powered for CPU container..."
 
