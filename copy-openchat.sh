4bd7837a2d46e73f5624ecc93f89e519181fd75b
IgorBeHolder <isorochan@gmail.com>
2023-12-23 14:12:29 +0300

    is_coder

--- copy-openchat.sh
+++ copy-openchat.sh
@@ -9,11 +9,19 @@ sudo chmod 755 "$BASE_DIR"
 
 # Download files and set permissions
 declare -a files=(
-  ".gitattributes" "README.md"  "added_tokens.json" "config.json" 
-  "generation_config.json" "openchat.json" "pytorch_model-00001-of-00002.bin" 
-  "pytorch_model-00002-of-00002.bin" "pytorch_model.bin.index.json"
-  "special_tokens_map.json" "tokenizer.json" 
-  "tokenizer_config.json" "tokenizer.model" 
+  ".gitattributes" 
+  "README.md"  
+  "added_tokens.json" 
+  "config.json" 
+  "generation_config.json" 
+  "model-00001-of-00003.safetensors" 
+  "model-00002-of-00003.safetensors" 
+  "model-00003-of-00003.safetensors" 
+  "model.safetensors.index.json" 
+  "openchat.json" 
+  "special_tokens_map.json" 
+  "tokenizer.model" 
+  "tokenizer_config.json" 
 )
 
 for file in "${files[@]}"
