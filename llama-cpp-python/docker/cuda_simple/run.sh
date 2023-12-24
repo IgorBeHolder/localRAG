1279e6085b7e54be691dcff6240887b57d09023d
IgorBeHolder <isorochan@gmail.com>
2023-12-24 17:01:44 +0300

    wip

--- llama-cpp-python/docker/cuda_simple/run.sh
+++ llama-cpp-python/docker/cuda_simple/run.sh
@@ -3,7 +3,7 @@
 echo -e "Environment variables loaded.\n"
 echo -e "*************************"
 echo -e "Build for $DEVICE.\n"
-echo -e "$COMPLETION_MODEL_NAME\nHOST: $HOST PORT: $MM_PORT n_ctx: $N_CTX\n
+echo -e "$COMPLETION_MODEL_NAME\nHOST: $HOST PORT: $MM_PORT n_ctx: $N_CTX\n"
 exho -e "*************************"
 
 python3 -m llama_cpp.server \
