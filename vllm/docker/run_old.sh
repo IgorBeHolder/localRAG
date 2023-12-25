7625f0c887522cc4868f22f86e2f9f6a857c67aa
IgorBeHolder <isorochan@gmail.com>
2023-12-25 11:24:45 +0300

    wip

--- vllm/docker/run_old.sh
+++ vllm/docker/run_old.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-python -m vllm.entrypoints.openai.api_server \
-    --model "../../model-store/$COMPLETION_MODEL_NAME" \
-    --port $MM_PORT \
-    --host $HOST
+# python -m vllm.entrypoints.openai.api_server \
+#     --model "../../model-store/$COMPLETION_MODEL_NAME" \
+#     --port $MM_PORT \
+#     --host $HOST
 
