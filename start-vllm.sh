d2e577b50b2462f4e4d9ee330a623dad800637da
IgorBeHolder <isorochan@gmail.com>
2023-12-26 15:10:08 +0300

    vllm

--- start-vllm.sh
+++ start-vllm.sh
@@ -14,13 +14,13 @@ git checkout main
 # Check if the "vllm-cont" container is running and stop it if it is
 container_id=$(docker ps -a -q -f name=^/vllm$)
 if [ ! -z "$container_id" ]; then
-    echo "Removing existing container with name 'vllm'..."
+    echo "***** Removing existing container with name 'vllm'..."
     docker rm -f $container_id
 fi
 # Check if the "llm-server" container is running and stop it if it is
 container_id=$(docker ps -a -q -f name=^/llm-server$)
 if [ ! -z "$container_id" ]; then
-    echo "Removing existing container with name 'llm-server'..."
+    echo "***** Removing existing container with name 'llm-server'..."
     docker rm -f $container_id
 fi
 
