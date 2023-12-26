9c2aaa378c17a3d3bca5ff8f881a5ada21f35840
IgorBeHolder <isorochan@gmail.com>
2023-12-26 09:58:24 +0300

    change coder prompt

--- start-coder.sh
+++ start-coder.sh
@@ -3,5 +3,13 @@
 (
 echo "Starting the DA processor..."
 
+container_id=$(docker ps -a -q -f name=^/coder$)
+if [ ! -z "$container_id" ]; then
+    echo "Stopping existing container with name 'anyth'..."
+    # docker rm -f $container_id
+    docker stop $container_id
+    docker rm $container_id
+fi
+
 cd coder && docker-compose -p localrag up -d --build
 ) 
