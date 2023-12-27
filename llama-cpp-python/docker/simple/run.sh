6c00fe484c973ba41411ae79e1b5ed431ccb1fef
IgorBeHolder <isorochan@gmail.com>
2023-12-27 22:23:34 +0300

    llama updates

---
+++ llama-cpp-python/docker/simple/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo $PWD
+printf "Environment variables loaded.\n"
+printf "*************************"
+printf "Build for $DEVICE.\n"
+printf "$COMPLETION_MODEL_NAME\nHOST: $HOST PORT: $MM_PORT n_ctx: $N_CTX.\n"
+printf "*************************"
+
+
+python3 -m llama_cpp.server \
+    --model "/app/model-store/$COMPLETION_MODEL_NAME" \
+    --host $HOST \
+    --port $MM_PORT \
+    --chat_format $CHAT_FORMAT \
+    --n_ctx $N_CTX
