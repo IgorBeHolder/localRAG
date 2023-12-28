b114282921e0a59915580275fae119e5be3c487e
nv <naverstay.me@gmail.com>
2023-12-28 10:37:27 +0100

    conversation chat

--- anything-llm/frontend/src/components/Modals/MangeWorkspace/Settings/index.jsx
+++ anything-llm/frontend/src/components/Modals/MangeWorkspace/Settings/index.jsx
@@ -172,7 +172,7 @@ export default function WorkspaceSettings({workspace}) {
               </div>
               <textarea
                 name="openAiPrompt"
-                maxLength={500}
+                maxLength={512}
                 rows={5}
                 defaultValue={workspace?.openAiPrompt ?? DEFAULT_CHAT_OPTIONS.openAiPrompt}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-stone-600 dark:border-stone-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
--- anything-llm/frontend/src/components/WorkspaceChat/ChatContainer/index.jsx
+++ anything-llm/frontend/src/components/WorkspaceChat/ChatContainer/index.jsx
@@ -260,6 +260,8 @@ export default function ChatContainer({workspace, isCoder, knownHistory = []}) {
     useEffect(() => {
       const promptMessage = chatHistory.length > 0 ? chatHistory[chatHistory.length - 1] : null;
 
+      console.log('chatHistory', workspace, promptMessage, chatHistory);
+
       async function fetchReply() {
         return await Workspace.sendChat(
           workspace,
