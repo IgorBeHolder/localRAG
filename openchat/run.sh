82cc5b9242b93dc4bf0f41edc0b96708659f544b
IgorBeHolder <isorochan@gmail.com>
2023-12-30 22:12:27 +0300

    ochat wip

--- openchat/run.sh
+++ openchat/run.sh
@@ -4,13 +4,13 @@ echo $PWD
 printf "Environment variables loaded.\n"
 printf "*************************"
 printf "Build for $DEVICE.\n"
-printf "ollm server: $COMPLETION_MODEL_NAME\nHOST: $HOST PORT: $PORT.\n"
+printf "ollm server: $COMPLETION_MODEL_NAME\nHOST: $HOST PORT: $MM_PORT.\n"
 printf "*************************"
 
 
 python -m ochat.serving.openai_api_server \
     --model=/model-store/${COMPLETION_MODEL_NAME} \
     --host $HOST \
-    --port $PORT
+    --port $MM_PORT
 
     # --model "/app/model-store/$COMPLETION_MODEL_NAME" \
