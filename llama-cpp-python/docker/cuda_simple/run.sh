a0a8a5671b811b098733a3770a645757a53f8a2b
IgorBeHolder <isorochan@gmail.com>
2023-12-31 17:41:22 +0300

    wip

--- llama-cpp-python/docker/cuda_simple/run.sh
+++ llama-cpp-python/docker/cuda_simple/run.sh
@@ -10,7 +10,6 @@ python3 -m llama_cpp.server \
     --model "/app/model-store/$COMPLETION_MODEL_NAME" \
     --host $HOST \
     --port $MM_PORT \
-    --tokenizer.ggml.add_bos_token false
     # --n_gpu_layers $N_GPU_LAYERS \
     # --chat_format $CHAT_FORMAT \
     # --n_ctx $N_CTX
