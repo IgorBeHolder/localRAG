028e6a068edc1cadb1da557ca317f89d1fc279dc
IgorBeHolder <isorochan@gmail.com>
2024-01-04 18:57:27 +0300

    mixtral awq

--- z_model/copy-mixtral-awq.sh
+++ z_model/copy-mixtral-awq.sh
@@ -14,8 +14,11 @@ declare -a files=(
   "config.json" 
   "generation_config.json"
   "model-00001-of-00003.safetensors"
+  # SHA256: 6251f54496d2946e844b2397b8b1cb45a16ea5327e189bf7ddacb82d24553790
   "model-00002-of-00003.safetensors"
+  # SHA256: a1233c8a0c379c11503565e773f0bacc1b8cafa582e22a356c92b61c913eab2f
   "model-00003-of-00003.safetensors"
+  # SHA256: 1073cbc34284f6e96916d9369718cb7bcbb174af451a5424535e29fab31eb019
   "model.safetensors.index.json"
   "quant_config.json"
   "special_tokens_map.json"
