a05d4c97f0253d6e818f7a383696ec7cfddb33f5
IgorBeHolder <isorochan@gmail.com>
2024-01-05 13:13:21 +0300

    mixtral awq

--- llama-cpp-python/docker/simple/run.sh
+++ llama-cpp-python/docker/simple/run.sh
@@ -12,5 +12,5 @@ python3 -m llama_cpp.server \
     --model "/app/model-store/$COMPLETION_MODEL_NAME" \
     --host $HOST \
     --port $MM_PORT \
-    --chat_format $CHAT_FORMAT \
+    --chat_template $CHAT_FORMAT \
     --n_ctx $N_CTX
