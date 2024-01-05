ab1ab5fc7f2bf5a58edf1be45bae20fd7beed63b
IgorBeHolder <isorochan@gmail.com>
2024-01-05 14:12:26 +0300

    mixtral llama-cpp

--- llama-cpp-python/docker/cuda_simple/run.sh
+++ llama-cpp-python/docker/cuda_simple/run.sh
@@ -11,10 +11,10 @@ python3 -m llama_cpp.server \
     --host $HOST \
     --port $MM_PORT \
     --n_gpu_layers $N_GPU_LAYERS \
-    --tensor_split $TP_SIZE \
     --main-gpu 0
     # --n_ctx $N_CTX
-
+    # --tensor_split $TP_SIZE \
+    
 # usage: __main__.py [-h] [--model MODEL] [--model_alias MODEL_ALIAS]
 #                    [--n_gpu_layers N_GPU_LAYERS] [--main_gpu MAIN_GPU]
 #                    [--tensor_split [TENSOR_SPLIT ...]]
